8841e2212a54fed468d40fc06643f18c565206ac
davidvjn <davidhouse1612@gmail.com>
2019-05-13 17:25:52 +0200

    suite

--- data/index.php
+++ data/index.php
@@ -1,1 +1,27 @@
-<?php echo 'salut';
+<?php echo 'salut';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script type="text/javascript" src="popup.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    
+
+    <form>
+<input type="button" value="Valider" onClick="Message()">
+</form>
+<script type="text/javascript">
+   function Message() {
+       var msg="Votre inscription est validée!";
+       //console.log(msg)
+       alert(msg);
+   }
+</script>
+</body>
+</html>
+
---
+++ data/popup.js
@@ -0,0 +1,10 @@
+<form>
+<input type="button" value="Valider" onClick="Message()">
+</form>
+<script type="text/javascript">
+   function Message() {
+       var msg="Votre inscription est validée!";
+       console.log(msg)
+       alert(msg);
+   }
+</script>
