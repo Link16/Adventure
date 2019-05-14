c45b35f93745106ffe090058fd773ba88c45d30f
davidvjn <49305618+davidvjn@users.noreply.github.com>
2019-05-14 17:44:34 +0200

    zouz

--- data/Inscription.php
+++ data/Inscription.php
@@ -54,6 +54,15 @@
     </div>
     <div class="row">
       <input type="submit" value="S'inscrire" onClick='Message()'>
+      <script type="text/javascript" src="./js/popup.js">
+  //  function Message() {
+  //      var msg="Votre inscription est validée!";
+  //      //console.log(msg)
+  //      alert(msg);
+  //  }
+  
+
+</script>
   </div>
 </div>
   </form>
--- data/js/popup.js
+++ data/js/popup.js
@@ -1,3 +1,4 @@
+<<<<<<< .merge_file_a47308
 function Message() {
        var msg="Votre inscription est validée!";
        console.log(msg)
@@ -6,3 +7,18 @@ function Message() {
    if (login == " ") {
        alert("Le formulaire n'est pas rempli correctement. Votre inscription n'est pas validée!")
    }
+=======
+    if (document.querySelectorAll('#name') == " ") {
+        function Message() {
+            var msg="Le formulaire n'est pas rempli correctement. Votre inscription n'est pas validée!";
+            alert(msg);
+        }
+}
+    else
+        function Message() {
+            var msg="Votre inscription est validée!";
+            console.log(msg)
+            alert(msg);
+    }
+    
+>>>>>>> .merge_file_a47460
