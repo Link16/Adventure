4e3fdee95931ba00ec58bc2601ddb33f9b0792b1
davidvjn <49305618+davidvjn@users.noreply.github.com>
2019-05-14 11:25:17 +0200

    index

--- data/index.php
+++ data/index.php
@@ -15,6 +15,7 @@
 </header>    
 
 <main>
+<!-- Bouton validation inscription JS -->
     <form>
 <input type="button" value="Valider" onClick="Message()">
 </form>
@@ -26,7 +27,13 @@
    }
 </script>
 
-
+<!-- Pitch du jeu -->
+<section>
+    <h1>WELCOME TO <strong>ADVENTURE</strong></h1>
+        <p>"La paix, le bonheur et la sérénité règnent dans le petit village de Trüperdu depuis près de 3 siècles.</p>
+        <p> Mais, au solstice de cette année, le village allait être confronté à un cataclysme terrible : la sorcière Marcelle était, comme tous les 326 ans, sur le point de se réveiller et de déclencher sa fureur sur le petit village.</p>
+        <p>Mamie Müjo, la doyenne du village, était préparée au retour de la sorcière. Elle fera passer aux jeunes de Trüperdu les épreuves de <strong>Sagesse, Agilité et Savoir</strong> afin de savoir qui pourrait stopper et anéantir la terrible sorcière Marcelle dans sa folie.</p>
+    <h1>SERAS-TU LE <strong>HEROS</strong> ou l' <strong>HEROÏNE</strong> DONT LE VILLAGE A BESOIN ?</h1>
 
     </main>
 
