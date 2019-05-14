50548a6a97e90edec6b94495f4d8d9faee6f9703
davidvjn <49305618+davidvjn@users.noreply.github.com>
2019-05-14 17:00:05 +0200

    pop

--- data/js/popup.js
+++ data/js/popup.js
@@ -1,9 +1,13 @@
-function Message() {
-       var msg="Votre inscription est validée!";
-       console.log(msg)
-       alert(msg);
-   }
-   
-    if (name['lastName', 'firstName', 'age', 'Login', 'Password'] == " ") {
-    alert("Le formulaire n'est pas rempli correctement. Votre inscription n'est pas validée!")
+    if (document.querySelectorAll('#name') == " ") {
+        function Message() {
+            var msg="Le formulaire n'est pas rempli correctement. Votre inscription n'est pas validée!";
+            alert(msg);
+        }
 }
+    else
+        function Message() {
+            var msg="Votre inscription est validée!";
+            console.log(msg)
+            alert(msg);
+    }
+    
