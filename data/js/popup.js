0e13040bac53fd7263eab5e8cb139f6747656b02
davidvjn <49305618+davidvjn@users.noreply.github.com>
2019-05-15 10:20:00 +0200

    sf

--- data/js/popup.js
+++ data/js/popup.js
@@ -1,4 +1,4 @@
-    if (document.querySelectorAll('#name') == " ") {
+    if (document.querySelectorAll('#name') == "") {
         function Message() {
             var msg="Le formulaire n'est pas rempli correctement. Votre inscription n'est pas validée!";
             alert(msg);
