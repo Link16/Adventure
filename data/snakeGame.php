f7b5b48cfee9db85943067168f9b5b39a928f662
davidvjn <davidhouse1612@gmail.com>
2019-05-16 19:39:56 +0200

    db

--- data/snakeGame.php
+++ data/snakeGame.php
@@ -1,11 +1,14 @@
+<?php session_start(); ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/main.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/episode.css">
+    <title>Epreuve 2</title>
 </head>
 <body>
     <div class="flex agilityDiv">
