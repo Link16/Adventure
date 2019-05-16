74e832f8c0ac4270d9beaa0c771aaa8f13f8a86b
davidvjn <davidhouse1612@gmail.com>
2019-05-16 19:24:29 +0200

    helleo

--- data/controllers/test.php
+++ data/controllers/test.php
@@ -1,8 +1,8 @@
 <?php 
 
-$pdo = new PDO('mysql:host=localhost; dbname=scores; charset=utf8', 'root', '0000');
-$stmt = $pdo->prepare('INSERT INTO scores (agilityScore) VALUES (:agilityScore)');
-$stmt->bindParam(':agilityScore', $_POST['agilityScore']);
+$pdo = new PDO('mysql:host=localhost; dbname=Scores; charset=utf8', 'root', '0000');
+$stmt = $pdo->prepare('UPDATE score VALUES score');
+$stmt->bindParam(':score', $_POST['score']);
 $stmt->execute();
 
 
