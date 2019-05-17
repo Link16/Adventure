328f0233e68708972a8db1ef1e1afda1a0332edd
davidvjn <davidhouse1612@gmail.com>
2019-05-17 08:08:50 +0200

    zer

--- data/fin.php
+++ data/fin.php
@@ -5,27 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/fin.css">
     <link rel="icon" href="./ressources/favico.ico"/>
     <title>Adventure</title>
-</head>
-<body>
-<?php include './templates/header.php';?>
-
-<!-- Pitch du jeu -->
-<embed name="GoodEnough" src="./ressources/trol.mp3" loop="true" hidden="true" autostart="true">
-<div class="containertroll">
-    <div class="hautdroittroll">
-  
-    </div>
-    <div class="hautgauchetroll">
-     
-    </div>  
-</div>
-
-
-
-    <?php include './templates/footer.html'; ?>
-
-</body>
+    <body>
+    <embed name="GoodEnough" src="./ressources/trol.mp3" loop="true" hidden="true" autostart="true">
+        <div id='middle'>
+            <h1 class="tracking-in-expand">! SURPRISE !</h1>
+        </div>
+        <div id='middle'>
+            <h1 class="tracking-in-expand">! YOU HAVE BEEN SCAMMED !</h1>
+        </div>
+        <div id="object" class="rotate-center">
+            <img src="./ressources/trol1.jpg">
+        </div>
+        <div id="zouzou" class="rotate-center">
+            <img src="./ressources/trol1.jpg">
+        </div>
+        <div id="objecttop" class="rotate-center">
+            <img src="./ressources/trol2.jpg">
+        </div>
+        <div id="zouzoutop" class="rotate-center">
+            <img src="./ressources/trol3.jpg">
+        </div>
+    </body>
 </html>
